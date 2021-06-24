b1390b5baadec1ebf2c732e148417c1e257df08b
Joseph Davis <joethebartenderpdx@gmail.com>
2021-06-24 16:53:28 -0700

    first fail!!

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,25 @@ describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('POST a new studio', async () => {
+    const res = await request(app)
+      .post('/api/v1/studios')
+      .send({
+        name: 'Studio J',
+        city: 'New York',
+        state: 'New York',
+        country: 'USA'
+      });
+
+    expect(res.body).toEqual({
+      id: 1,
+      name: 'Studio J',
+      city: 'New York',
+      state: 'New York',
+      country: 'USA',
+      updatedAt: expect.any(String),
+      createdAt: expect.any(String)
+    });
+  });
 });
